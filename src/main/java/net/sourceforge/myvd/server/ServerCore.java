5da776e3b1fb03b8ab6e35564c3538c688f7222d
Tomas Tulka <tomas.tulka@gmail.com>
2018-06-05 08:38:59 +0200

    default value for server.globalChain property

--- src/main/java/net/sourceforge/myvd/server/ServerCore.java
+++ src/main/java/net/sourceforge/myvd/server/ServerCore.java
@@ -21,8 +21,6 @@ import java.util.Iterator;
 import java.util.Properties;
 import java.util.StringTokenizer;
 
-import org.apache.logging.log4j.Logger;
-
 import net.sourceforge.myvd.core.InsertChain;
 import net.sourceforge.myvd.core.NameSpace;
 import net.sourceforge.myvd.inserts.Insert;
@@ -33,153 +31,134 @@ import net.sourceforge.myvd.util.SchemaUtil;
 import com.novell.ldap.LDAPException;
 import com.novell.ldap.util.DN;
 
+import org.apache.logging.log4j.Logger;
+
 public class ServerCore {
-	static Logger logger = org.apache.logging.log4j.LogManager.getLogger(ServerCore.class);
-	
-	Properties props;
-	private InsertChain globalChain;
-	private Router router;
-	
+    static Logger logger = org.apache.logging.log4j.LogManager.getLogger(ServerCore.class);
 
+    Properties props;
+    private InsertChain globalChain;
+    private Router router;
 
-	private NameSpace globalNS;
+    private NameSpace globalNS;
 
+    public ServerCore(Properties props) {
+        this.props = props;
+    }
 
-	
-	public ServerCore(Properties props) {
-		this.props = props;
-	}
+    public InsertChain getGlobalChain() {
+        return globalChain;
+    }
 
-	public InsertChain getGlobalChain() {
-		return globalChain;
-	}
+    public Properties getProps() {
+        return props;
+    }
 
-	public Properties getProps() {
-		return props;
-	}
+    public Router getRouter() {
+        return router;
+    }
 
-	public Router getRouter() {
-		return router;
-	}
+    private void configureChain(String prefix, ArrayList<String> links, InsertChain chain, NameSpace ns) throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
+        Iterator<String> it = links.iterator();
+        int i = 0;
 
-	
-	
-	
+        while (it.hasNext()) {
+            String name = it.next();
+            chain.setInsert(i, chain.getInsertConfig(name, prefix + name + ".", chain, i));
 
-	private void configureChain(String prefix,ArrayList<String> links,InsertChain chain,NameSpace ns) throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
-		Iterator<String> it = links.iterator();
-		int i=0;
-		
-		while (it.hasNext()) {
-			String name = it.next();
-			chain.setInsert(i, chain.getInsertConfig(name,prefix +  name + ".",chain,i));
-			
-			i++;
-		}
-		
-		chain.configureChain();
-	}
-	
-	private void buildGlobalChain() throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
-		String links = props.getProperty("server.globalChain");
-		ArrayList<String> linkList = new ArrayList<String>();
-		
-		StringTokenizer toker = new StringTokenizer(links,",");
-		
-		while (toker.hasMoreTokens()) {
-			linkList.add(toker.nextToken());
-		}
-		
-		Insert[] tchain = new Insert[linkList.size()];
-		InsertChain chain = new InsertChain(tchain);
-		this.globalNS = new NameSpace("globalChain",new DistinguishedName("cn=root"),0,chain,true);
-		chain.setNameSpace(this.globalNS);
-		chain.setProps(this.props);
-		
-		this.configureChain("server.globalChain.",linkList,chain,this.globalNS);
-		
-		this.globalChain = chain;
-		
-		
-		
-	}
-	
-	private void buildNamespaces() throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
-		String nss = props.getProperty("server.nameSpaces");
-		StringTokenizer toker = new StringTokenizer(nss,",");
-		Router router = new Router(this.globalChain);
+            i++;
+        }
 
-		
-		
-		while (toker.hasMoreTokens()) {
-			
-			
-			String nsName = toker.nextToken();
-			
-			
-			
-			logger.debug("Loading namespace : " + nsName);
-			
-			String prefix = "server." + nsName + ".";
-			int weight = Integer.parseInt(props.getProperty(prefix + "weight","0"));
-			String nsBase = props.getProperty(prefix + "nameSpace");
-			
-			boolean enabled = props.getProperty(prefix + "enabled","true").equalsIgnoreCase("true");
-			
-			if (! enabled) {
-				logger.warn("Namespace " + nsName + " disabled");
-				
-			} else {
-			
-				String nsChain = props.getProperty(prefix + "chain");
-				StringTokenizer chainToker = new StringTokenizer(nsChain,",");
-				
-				ArrayList<String> chainList = new ArrayList<String>();
-				
-				while (chainToker.hasMoreTokens()) {
-					chainList.add(chainToker.nextToken());
-				}
-				
-				Insert[] tchain = new Insert[chainList.size()];
-				InsertChain chain = new InsertChain(tchain);
-				chain.setProps(props);
-				
-				NameSpace ns = new NameSpace(nsName,new DistinguishedName(nsBase),weight,chain,false);
-				chain.setNameSpace(ns);
-				ns.setRouter(router);
-				this.configureChain(prefix,chainList,chain,ns);
-				
-				router.addBackend(nsName,new DN(nsBase),ns);
-			}
-		}
-		
-		
-		
-		
-		this.router = router;
-	}
-	
-	public void startService() throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
-		logger.debug("Initializing Server wide config...");
-		this.buildServerWideConfig();
-		logger.debug("Loading global chain...");
-		this.buildGlobalChain();
-		logger.debug("Global chain loaded");
-		logger.debug("Loading local chain...");
-		this.buildNamespaces();
-		logger.debug("Local chain loaded");
-	}
+        chain.configureChain();
+    }
 
-	private void buildServerWideConfig() {
-		String binaryAttrs = props.getProperty("server.binaryAttributes","objectguid,orclguid,entryuuid");
-		StringTokenizer toker = new StringTokenizer(binaryAttrs,",",false);
-		while (toker.hasMoreTokens()) {
-			SchemaUtil.getSchemaUtil().addBinaryAttribute(toker.nextToken().toLowerCase());
-		}
-		
-		
-		
-	}
-	
-	
+    private void buildGlobalChain() throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
+        String links = props.getProperty("server.globalChain", "");
+        ArrayList<String> linkList = new ArrayList<String>();
+
+        StringTokenizer toker = new StringTokenizer(links, ",");
+
+        while (toker.hasMoreTokens()) {
+            linkList.add(toker.nextToken());
+        }
+
+        Insert[] tchain = new Insert[linkList.size()];
+        InsertChain chain = new InsertChain(tchain);
+        this.globalNS = new NameSpace("globalChain", new DistinguishedName("cn=root"), 0, chain, true);
+        chain.setNameSpace(this.globalNS);
+        chain.setProps(this.props);
+
+        this.configureChain("server.globalChain.", linkList, chain, this.globalNS);
+
+        this.globalChain = chain;
+
+    }
+
+    private void buildNamespaces() throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
+        String nss = props.getProperty("server.nameSpaces");
+        StringTokenizer toker = new StringTokenizer(nss, ",");
+        Router router = new Router(this.globalChain);
+
+        while (toker.hasMoreTokens()) {
+
+            String nsName = toker.nextToken();
+
+            logger.debug("Loading namespace : " + nsName);
+
+            String prefix = "server." + nsName + ".";
+            int weight = Integer.parseInt(props.getProperty(prefix + "weight", "0"));
+            String nsBase = props.getProperty(prefix + "nameSpace");
+
+            boolean enabled = props.getProperty(prefix + "enabled", "true").equalsIgnoreCase("true");
+
+            if (!enabled) {
+                logger.warn("Namespace " + nsName + " disabled");
+
+            } else {
+
+                String nsChain = props.getProperty(prefix + "chain");
+                StringTokenizer chainToker = new StringTokenizer(nsChain, ",");
+
+                ArrayList<String> chainList = new ArrayList<String>();
+
+                while (chainToker.hasMoreTokens()) {
+                    chainList.add(chainToker.nextToken());
+                }
+
+                Insert[] tchain = new Insert[chainList.size()];
+                InsertChain chain = new InsertChain(tchain);
+                chain.setProps(props);
+
+                NameSpace ns = new NameSpace(nsName, new DistinguishedName(nsBase), weight, chain, false);
+                chain.setNameSpace(ns);
+                ns.setRouter(router);
+                this.configureChain(prefix, chainList, chain, ns);
+
+                router.addBackend(nsName, new DN(nsBase), ns);
+            }
+        }
+
+        this.router = router;
+    }
+
+    public void startService() throws InstantiationException, IllegalAccessException, ClassNotFoundException, LDAPException {
+        logger.debug("Initializing Server wide config...");
+        this.buildServerWideConfig();
+        logger.debug("Loading global chain...");
+        this.buildGlobalChain();
+        logger.debug("Global chain loaded");
+        logger.debug("Loading local chain...");
+        this.buildNamespaces();
+        logger.debug("Local chain loaded");
+    }
+
+    private void buildServerWideConfig() {
+        String binaryAttrs = props.getProperty("server.binaryAttributes", "objectguid,orclguid,entryuuid");
+        StringTokenizer toker = new StringTokenizer(binaryAttrs, ",", false);
+        while (toker.hasMoreTokens()) {
+            SchemaUtil.getSchemaUtil().addBinaryAttribute(toker.nextToken().toLowerCase());
+        }
+
+    }
+
 }
