2d1bbc14ad5ed3f7eb990164409ddc2651ec4e6d
Tomas Tulka <tomas.tulka@gmail.com>
2018-06-08 12:40:38 +0200

    test env var removed

--- setenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export PROJ_DIR=`pwd`
-export TMP_DIR=/tmp/myvd
-export SCHEMA_DIR=$PROJ_DIR/test/ADPosixSim
-export SLAPD_PATH=$PROJ_DIR/bin
-export DEBUG_SERVER=true
-export JAVA_HOME=/usr/lib/jvm/default-java
