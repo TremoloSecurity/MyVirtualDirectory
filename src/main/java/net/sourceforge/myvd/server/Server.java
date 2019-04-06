af836090611f931be7c3edc6ab906e363adfd6c5
mlbiam <marc@tremolo.io>
2019-04-06 14:02:13 -0400

    for #56 set version

--- src/main/java/net/sourceforge/myvd/server/Server.java
+++ src/main/java/net/sourceforge/myvd/server/Server.java
@@ -71,7 +71,7 @@ public class Server {
     static Logger logger;
 
 
-	public final static String VERSION = "1.0.5";
+	public final static String VERSION = "1.0.6";
 
     String configFile;
     Properties props;
