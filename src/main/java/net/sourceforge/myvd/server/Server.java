b54cf3bfde98422dc690283fd1b778489c1f2ee4
Marc Boorshtein <marc.boorshtein@tremolosecurity.com>
2024-12-12 14:26:16 -0500

    for #167 bumped versions and dependencies

--- src/main/java/net/sourceforge/myvd/server/Server.java
+++ src/main/java/net/sourceforge/myvd/server/Server.java
@@ -78,7 +78,7 @@ public class Server {
     static Logger logger = Logger.getLogger(Server.class);
 
 
-	public final static String VERSION = "1.0.22";
+	public final static String VERSION = "1.0.23";
 
     String configFile;
     Properties props;
